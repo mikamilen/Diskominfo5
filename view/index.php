c3dab5e7857fca4b18f73a03eeb771396605f6ba
Mika Milen <mika.milennius@gmail.com>
2017-11-14 21:50:49 +0700

    Edit home

---
+++ view/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+    
+    <link rel="stylesheet" href="/Diskominfo5/asset/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="/Diskominfo5/asset/css/home.css" type="text/css">
+</head>
+<body>
+
+<div class="home-screen">
+  <img src="/Diskominfo5/asset/images/home.png">
+  <div class="home-main">
+    <h1>Bandung Smart City</h1>
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search" aria-describedby="basic-addon2">
+          <span class="input-group-btn">
+          <input type="submit" value="Search" class="btn btn-default">
+          </span>
+      </div>
+  </div>
+</div>
+
+
+<div class="smart-container">
+  <div class="col-md-12" id="b">
+  
+  <div class="row">
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Smart 1</h3>
+        <p>...</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Smart 2</h3>
+        <p>...</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Smart 3</h3>
+        <p>...</p>
+      </div>
+    </div>
+  </div>
+    <div class="col-md-6 text">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    </div>
+  </div>
+  </div>
+
+
+  <div class="col-md-12" id="b">
+
+  <div class="row">
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Smart 4</h3>
+        <p>...</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Smart 5</h3>
+        <p>...</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-2">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Smart 6</h3>
+        <p>...</p>
+      </div>
+    </div>
+  </div>
+
+
+    <div class="col-md-6 text">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.possimus obcaecati. 
+    </div>
+    </div>
+
+  </div>
+</div>
+<!-- 
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src=/Diskominfo5/asset/images/download.png>
+      <div class="caption">
+        <h3>Judul</h3>
+        <p>Text</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src=/Diskominfo5/asset/images/download.png>
+      <div class="caption">
+        <h3>Judul</h3>
+        <p>Text</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src=/Diskominfo5/asset/images/download.png>
+      <div class="caption">
+        <h3>Judul</h3>
+        <p>Text</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<div class="margin">
+
+  <div class="col-sm-12 col-md-8">
+    <div class="thumbnail">
+      <img src=/Diskominfo5/asset/images/download.png width="800" height="500" id="image">
+      <div class="caption">
+        <h3>Acara Terhangat</h3>
+        <p>Text</p>
+      </div>
+    </div>
+  </div>
+
+    <div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src=/Diskominfo5/asset/images/download.png alt="...">
+      <div class="caption">
+          <h3>Acara</h3>
+        <p>Text</p>
+          <p><a href="#" class="btn btn-primary" role="button">Selengkapnya</a></p>
+      </div>
+    </div>
+  </div>
+        <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src=/Diskominfo5/asset/images/download.png alt="...">
+      <div class="caption">
+          <h3>Acara</h3>
+        <p>Text</p>
+          <p><a href="#" class="btn btn-primary" role="button">Selengkapnya</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+    
+</div>    
+  <script src="/Diskominfo5/asset/js/bootstrap.min.js" type="text/javascript"></script>
+
+
+</body>
+</html>
+
+     -->
