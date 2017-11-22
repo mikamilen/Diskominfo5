cc1947509f59e05a9fec5c1d2c7c36873fd14503
Mika Milen <mika.milennius@gmail.com>
2017-11-22 15:53:11 +0700

    Add profil dan blogppost

---
+++ view/blogpost.php
@@ -0,0 +1,69 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="/Diskominfo5/asset/bootstrap/css/bootstrap.min.css" type="text/css">
+        <link rel="stylesheet" href="/Diskominfo5/asset/css/blogpost.css" type="text/css">
+    </head>
+    <body>
+        <center class="col-lg-8"><h2>Judul</h2></center>
+        <div class="content col-lg-8">
+            <p>
+                <h4 style="margin-left:20px"><?= date('d M Y')?></h4>
+            </p>
+            <table style="margin-left:20px" border="1">
+                <td>
+                    <th width="1000px" height="400px"></th>
+                </td>
+            </table>
+            <p style="margin-left:20px"><b>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in
+            </b></p>
+            <p style="margin-left:20px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p style="margin-left:20px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p style="margin-left:20px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <p>
+            Bagikan : 
+            <img src=/Diskominfo5/asset/images/g+.png width="30" height="30">
+            <img src=/Diskominfo5/asset/images/facebook.png width="30" height="30">
+            <img src=/Diskominfo5/asset/images/twiter.png width="30" height="30">
+        </p>
+        </div>
+        
+        <div class="content col-lg-4">
+            <h4>Text</h4>
+            <table border="1">
+                <td>
+                    <th width="400px" height="200px"></th>
+                </td>
+            </table><br>
+            <h4>Text</h4>
+            <div class="col-lg-6">
+            <table border="1">
+                <td>
+                    <th width="200px" height="100px"></th>
+                </td>
+            </table>
+            </div>
+            <h5>Text</h5>
+            <p><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b></p><br><br>
+            <div class="col-lg-6">
+            <table border="1">
+                <td>
+                    <th width="200px" height="100px"></th>
+                </td>
+            </table>
+            </div>
+            <h5>Text</h5>
+            <p><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b></p><br><br>
+            <div class="col-lg-6">
+            <table border="1">
+                <td>
+                    <th width="200px" height="100px"></th>
+                </td>
+            </table>
+            </div>
+            <h5>Text</h5>
+            <p><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b></p>
+        </div>
+        
+    </body>
+</html>
---
+++ view/profil.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+    
+    <link rel="stylesheet" href="/Diskominfo5/asset/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="/Diskominfo5/asset/css/profil.css" type="text/css">
+</head>
+<body>
+
+<div class="home-screen">
+  <img src="/Diskominfo5/asset/images/home.png">
+  </div>
+</div>
+    <div class="profil-content">
+        <h1><center>Smart City Kota Bandung</center></h1>
+        
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    
+    </div>
+    
+    <div class="col-lg-3"></div>
+    <div class="col-lg-6">
+        <div class="col-lg-4">
+            <center>
+            <table border="1">
+                <td>
+                    <th width="20px" height="20px"></th>
+                </td>
+            </table>
+            <p>Gambar</p>
+            </center>
+        </div>
+        <div class="col-lg-4">
+            <center>
+            <table border="1">
+                <th width="20px" height="20px"></th>
+            </table>
+            <p>Gambar</p>
+            </center>
+        </div>
+        <div class="col-lg-4">
+            <center>
+            <table border="1">
+                <th width="20px" height="20px"></th>
+            </table>
+            <p>Gambar</p>
+            </center>
+        </div>
+    </div>
+    <div class="col-lg-3"></div><br><br><br>
+    
+    <div class="profil-content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+    <div class="col-lg-3"></div>
+    <div class="col-lg-6">
+        <div class="col-lg-4">
+            <center>
+            <img src=/Diskominfo5/asset/images/facebook.png width="30" height="30">
+            </center>
+        </div>
+        <div class="col-lg-4">
+            <center>
+            <img src=/Diskominfo5/asset/images/gmail.png width="30" height="30">
+            </center>
+        </div>
+        <div class="col-lg-4">
+            <center>
+            <img src=/Diskominfo5/asset/images/instagram3.png width="30" height="30">
+            </center>
+        </div>
+    </div>
+    <div class="col-lg-3"></div><br><br>
+
+    <div class="profil-content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque beatae, sed laudantium sequi error, odit, aut explicabo consequatur in consequuntur sapiente vero laborum iure distinctio, facilis qui possimus obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</div>
+</html>
